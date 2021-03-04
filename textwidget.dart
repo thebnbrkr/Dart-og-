bfd94bdf7f4eb7b8ce5fc9f283c82e2d245ffa1b
thebnbrkr <51245304+thebnbrkr@users.noreply.github.com>
2021-03-04 17:10:35 +0530

    Create textwidget.dart

---
+++ textwidget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    //using google material app widget
+    home: Text("thebnbrkr"),
+  )); //importings stuff from MyApp
+}
